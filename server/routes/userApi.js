0af03ce2d5af1c6e69971a68c1d36598392233e1
NickVik04 <nikhilshrm49@gmail.com>
2022-09-28 21:33:25 +0530

    Creating User API

--- server/routes/userApi.js
+++ server/routes/userApi.js
@@ -1,7 +1,22 @@
 const express = require("express");
 const router = express.Router();
-
+const {check, validationResult} = require("express-validator");
 
 router.get("/",(req,res) => res.send("User route"));
+router.post("/",
+[
+    check("name","Name is Required").not().isEmpty(),
+    check("email","Enter a Valid Email").isEmail(),
+    check("passowrd", "Password should have atleast 5 charactes").isLength({min:5})
+],
+(req,res)=>{
+    const errors = validationResult(req);
+    console.log(req.body);
+    if(!errors.isEmpty()) {
+        return res.status(400).json({errors: errors.array()});
+    }
+
+
+});
 
 module.exports= router;
